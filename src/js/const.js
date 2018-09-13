9a223241b4f432feb25c38ae685a88011ddf52eb
Krishna Raj <krishna_raj@in.sysvine.com>
2018-09-13 20:49:07 +0530

    Initial commit for sendbird poc

---
+++ src/js/const.js
@@ -0,0 +1,14 @@
+export const APP_ID = '290590A1-09D6-4835-B384-4469A3997418';
+export const USER_ID = 'user_id';
+export const DISPLAY_NONE = 'none';
+export const DISPLAY_BLOCK = 'block';
+export const DISPLAY_FLEX = 'flex';
+export const ACTIVE_CLASSNAME = 'active';
+export const KEY_ENTER = 13;
+export const FILE_ID = 'attach_file_id';
+export const UPDATE_INTERVAL_TIME = 5 * 1000;
+export const COLOR_RED = '#DC5960';
+export const MESSAGE_REQ_ID = 'reqId';
+export const OPEN_CHANNEL_SEARCH_URL = 'search_open_channel';
+
+export const body = document.querySelector('body');
